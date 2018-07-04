f00a5747e6ac1e97212e974e05845aa3c742888b
canceryoon <photo9609@gmail.com>
2018-07-04 04:07:31 +0000

    Minimun Swaps 2

---
+++ hackerrank/MinimumSwaps2.cpp
@@ -0,0 +1,13 @@
+int minimumSwaps(vector<int> arr) {
+    int i,c=0,n=arr.size();
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==(i+1))
+            continue;
+        
+        swap(arr[i],arr[arr[i]-1]);
+        c++;
+        i--;
+    }
+    return c;
+}
